cea19ea81f5becd3297f88c23a8ea7ed46822000
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-12-28 22:48:02 +0600

    Done for today

--- WorkManager/app/src/main/java/com/github/workmanager/MainActivity.kt
+++ WorkManager/app/src/main/java/com/github/workmanager/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.github.workmanager
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,11 +10,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.work.Constraints
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.github.workmanager.ui.theme.WorkManagerTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var workManager: WorkManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        workManager = WorkManager.getInstance(applicationContext)
+
         setContent {
             WorkManagerTheme {
                 // A surface container using the 'background' color from the theme
@@ -24,4 +37,28 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val uri = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            intent?.getParcelableArrayExtra(Intent.EXTRA_STREAM, Uri::class.java)
+        } else {
+            intent?.getParcelableArrayExtra(Intent.EXTRA_STREAM)
+        } ?: return
+
+        val request = OneTimeWorkRequestBuilder<PhotoCompressionWorker>()
+            .setInputData(
+                workDataOf(
+                    PhotoCompressionWorker.KEY_CONTENT_URI to uri.toString(),
+                    PhotoCompressionWorker.KEY_COMPRESSION_THRESHOLD to 1024 * 20L
+                )
+            )
+            .setConstraints( Constraints(
+                requiresStorageNotLow = true
+            ))
+            .build()
+        workManager.enqueue(request)
+    }
+
+    // Continue from 24:25
 }
