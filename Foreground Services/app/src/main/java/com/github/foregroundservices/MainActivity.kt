73efe8992bb5e2bed406856623054de854808d60
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-12-28 13:26:28 +0600

    Working ..

--- Foreground Services/app/src/main/java/com/github/foregroundservices/MainActivity.kt
+++ Foreground Services/app/src/main/java/com/github/foregroundservices/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.github.foregroundservices
 
 import android.Manifest
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
 import com.github.foregroundservices.ui.theme.ForegroundServicesTheme
@@ -27,8 +32,32 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ForegroundServicesTheme {
-                Column {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Button(
+                        onClick = {
+                            Intent(applicationContext, RunningService::class.java).also {
+                                it.action = RunningService.Actions.START.toString()
+                                startService(it)
+                            }
+                        }
+                    ) {
+                        Text(text = "Start run")
+                    }
 
+                    Button(
+                        onClick = {
+                            Intent(applicationContext, RunningService::class.java).also {
+                                it.action = RunningService.Actions.STOP.toString()
+                                startService(it)
+                            }
+                        }
+                    ) {
+                        Text(text = "Stop run")
+                    }
                 }
             }
         }
