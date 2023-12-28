9536a088b548249a5b7f50e71f2b9b2d77e44ec2
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-12-28 12:47:48 +0600

    Saving the progress ...

--- Foreground Services/app/src/main/java/com/github/foregroundservices/RunningService.kt
+++ Foreground Services/app/src/main/java/com/github/foregroundservices/RunningService.kt
@@ -3,6 +3,7 @@ package com.github.foregroundservices
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 
 class RunningService: Service() {
     override fun onBind(intent: Intent?): IBinder? {
@@ -20,7 +21,13 @@ class RunningService: Service() {
     }
 
     private fun start(){
-        // Video duration: 5:37
+        val notification = NotificationCompat.Builder(this, "Primary_notification")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Run is active")
+            .setContentText("Elapsed time: 00:50")
+            .build()
+
+        startForeground(1, notification)
     }
 
     enum class Actions {
