cbd13d9c0587c11759fd6eac71d00f220eec8e8b
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-12-28 13:18:27 +0600

    Requesting runtime permission

--- Foreground Services/app/src/main/java/com/github/foregroundservices/MainActivity.kt
+++ Foreground Services/app/src/main/java/com/github/foregroundservices/MainActivity.kt
@@ -1,24 +1,34 @@
 package com.github.foregroundservices
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
 import com.github.foregroundservices.ui.theme.ForegroundServicesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                0
+            )
+        }
+
         setContent {
             ForegroundServicesTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    // Code from here
+                Column {
+
                 }
             }
         }
