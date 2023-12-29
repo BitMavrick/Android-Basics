783f31f47ee9fed3eecc5ba76c0dbb41fffdacfd
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-12-29 22:18:07 +0600

    Done with the photo viewModel

---
+++ WorkManager/app/src/main/java/com/github/workmanager/PhotoViewModel.kt
@@ -0,0 +1,30 @@
+package com.github.workmanager
+
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import java.util.UUID
+
+class PhotoViewModel: ViewModel() {
+    var uncompressedUri: Uri? by mutableStateOf(null)
+        private set
+    var compressedBitmap: Bitmap? by mutableStateOf(null)
+        private set
+    var workId: UUID? by mutableStateOf(null)
+        private set
+
+    fun updateCompressedUri(uri: Uri?){
+        uncompressedUri = uri
+    }
+
+    fun updateCompressedBitmap(bitmap: Bitmap?){
+        compressedBitmap = bitmap
+    }
+
+    fun updateWorkId(uuid: UUID){
+        workId = uuid
+    }
+}
